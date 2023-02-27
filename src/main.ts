3434f59282f4f269baeb232cdee281e2d0a81996
dieg0code <diegoobando20@gmail.com>
2023-02-27 05:11:35 -0300

    docker compose

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Configura el prefijo api/v2 para todas las rutas, es decir, todas las rutas tendrán el prefijo api/v2: "api/v2/pokemons"
+  app.setGlobalPrefix('api/v2');
   await app.listen(3000);
 }
 bootstrap();
