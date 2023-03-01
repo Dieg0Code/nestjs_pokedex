cec121e453bd51a80d0cda9b3f9fabf4cca359e6
dieg0code <diegoobando20@gmail.com>
2023-03-01 19:50:02 -0300

    mongo connection and db schema created

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,16 @@ import { join } from 'path';
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { PokemonModule } from './pokemon/pokemon.module';
+import { MongooseModule } from '@nestjs/mongoose';
 @Module({
   imports: [
     // Serve static files from the public directory
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
+    // npm i @nestjs/mongoose mongoose
+    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+
     PokemonModule,
   ],
 })
--- src/pokemon/entities/pokemon.entity.ts
+++ src/pokemon/entities/pokemon.entity.ts
@@ -1,1 +1,22 @@
-export class Pokemon {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+//Este decorador indica que es un esquema de mongoose
+@Schema()
+export class Pokemon extends Document {
+  // Document es una interfaz de mongoose, nos permite usar los metodos de mongoose
+  // id: string mongo lo añade automaticamente
+  @Prop({
+    unique: true,
+    index: true,
+  })
+  name: string;
+  @Prop({
+    unique: true,
+    index: true,
+  })
+  number: number;
+}
+
+// Se exporta un esquema creado a partir de la clase Pokemon
+export const PokemonSchema = SchemaFactory.createForClass(Pokemon);
